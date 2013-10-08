92a53d64590b0903bba339a62dac59bd28bf0d6d
WonderBeat <borov.htid@gmail.com>
2013-10-08 16:11:56 +0400

    Send auth to cross domain

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('tapcatWebApp', ['ngRoute'])
+	.config(['$httpProvider', function($httpProvider) {
+		$httpProvider.defaults.withCredentials = true;
+	}])
   .config(function ($routeProvider) {
     $routeProvider
       .otherwise({
